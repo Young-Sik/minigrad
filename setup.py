e76e181c0eaca4bea3f9a9318e807f1e15ca6ad4
Young-Sik Choi <youngsikmail@gmail.com>
2025-05-27 01:13:46 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='minigrad',
-    version='0.1',
-    packages=find_packages(),
-    install_requires=[
-        'numpy',
-        'pytest',
-    ],
+    name="minigrad",
+    version="0.1.0",
     author='Young-Sik Choi',
-    description='Educational minigrad autograd engine',
+    author_email="your.email@example.com",
+    description="A lightweight autograd engine for educational purposes.",
     organization='Korea Aerospace University',
-    license='MIT',
+    url="https://github.com/Young-Sik/minigrad",
+    packages=find_packages(include=["minigrad", "minigrad.*"]),
+    install_requires=[
+        "numpy"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
 )
